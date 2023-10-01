ffd7fd3e83cd060f41d4ab119205f181e7cf29f3
PixelHues098 <mobiemocken0978@gmail.com>
2023-10-01 12:11:52 +0800

    responsive sidebar with functional dark mode

--- script.js
+++ script.js
@@ -5,6 +5,20 @@ const body = document.querySelector("body"),
       modeSwitch = body.querySelector(".toggle-switch"),
       modeText = body.querySelector(".mode-text");
 
+      toggle.addEventListener("click", () =>{
+        sidebar.classList.toggle("close");
+      });
+      searchBtn.addEventListener("click", () =>{
+        sidebar.classList.remove("close");
+      });
+
+
       modeSwitch.addEventListener("click", () =>{
         body.classList.toggle("dark");
+
+        if(body.classList.contains("dark")){
+            modeText.innerText = "Light Mode"
+        }else{
+            modeText.innerText = "Dark Mode"
+        }
       });
